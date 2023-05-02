7f0d9c240413d0c725e6714f68e28433c37f1f09
unknown <joinej18.me@gmail.com>
2023-05-02 15:53:10 -0500

    Portfolio update

--- dist/js/typewriter.js
+++ dist/js/typewriter.js
@@ -2,7 +2,7 @@ class TypeWriter {
   constructor(txtElement, words, wait = 3000) {
     this.txtElement = txtElement;
     this.words = words;
-    this.txt = '';
+    this.txt = "";
     this.wordIndex = 0;
     this.wait = parseInt(wait, 10);
     this.type();
@@ -40,7 +40,7 @@ class TypeWriter {
       typeSpeed = this.wait;
       // Set delete to true
       this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+    } else if (this.isDeleting && this.txt === "") {
       this.isDeleting = false;
       // Move to next word
       this.wordIndex++;
@@ -53,13 +53,93 @@ class TypeWriter {
 }
 
 // Init On DOM Load
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 // Init App
 function init() {
-  const txtElement = document.querySelector('.txt-type');
-  const words = JSON.parse(txtElement.getAttribute('data-words'));
-  const wait = txtElement.getAttribute('data-wait');
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
+// Slider
+
+const slider = function () {
+  //Variables
+  const slides = document.querySelectorAll(".slide");
+  const buttonLeft = document.querySelector(".slider__btn--left");
+  const buttonRight = document.querySelector(".slider__btn--right");
+  const dotContainer = document.querySelector(".dots");
+  let curSlide = 0; //variable for the current slide
+  const maxSlide = slides.length;
+  //FUNCTIONS - SLIDER
+  const createDots = function () {
+    //create one element for each of the slides
+    slides.forEach(function (s, i) {
+      //the underscore is the throw away variable
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+  const activateDots = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+  //NEXT SLIDE FUNCTIONS
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) curSlide = 0;
+    else curSlide++;
+
+    goToSlide(curSlide);
+    activateDots(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) curSlide = maxSlide - 1;
+    else curSlide--;
+
+    goToSlide(curSlide);
+    activateDots(curSlide);
+  };
+
+  const init = function () {
+    createDots();
+    goToSlide(0);
+    activateDots(0);
+  };
+
+  init();
+  //EVENT HANDLERS
+  buttonRight.addEventListener("click", nextSlide);
+  buttonLeft.addEventListener("click", prevSlide);
+
+  //ADDING KEYBOARD EVENT
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+
+    //if (e.key === 'ArrowRight') prevSlide(); //can also use short circuiting
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset; //because it is called data slide
+      goToSlide(slide);
+      activateDots(slide);
+    }
+  });
+};
+slider();
